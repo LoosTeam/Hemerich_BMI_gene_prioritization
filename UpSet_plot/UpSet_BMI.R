a5bd65e78b03673a942e3417bb871e6c64f6632e
DaianeH <daianehemerich@gmail.com>
2024-02-29 08:23:10 -0500

    R code used to create UpSet plot

--- UpSet_plot/UpSet_BMI.R
+++ UpSet_plot/UpSet_BMI.R
@@ -1,1 +1,16 @@
 
+# R code used to create UpSet plot
+
+library(UpSetR)
+library(ggupset)
+library(ggplot2)
+library(ragg)
+ 
+setwd("/Users/hemericd/Downloads/upset/")
+venn_results_bmi <- read.table("venn_result7408.txt", stringsAsFactors = F, header = T, sep = '\t')
+venn_results_bmi$Tools <- as.list(strsplit(venn_results_bmi$Tools, " "))
+ 
+p <- ggplot(venn_results_bmi, aes(x=Tools)) +
+  geom_bar() +
+  geom_text(stat='count', aes(label=after_stat(count)), vjust=-1, size=3) +
+  scale_x_upset(reverse = TRUE, order_by = "degree")
